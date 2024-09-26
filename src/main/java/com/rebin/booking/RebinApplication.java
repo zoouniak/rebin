eb0afb34ab710740b1681a6c89cdcc78a5760f85
jueun oh <jueun1025@naver.com>
2024-09-26 10:15:26 +0900

    init : 프로젝트 초기 설정

---
+++ src/main/java/com/rebin/booking/RebinApplication.java
@@ -0,0 +1,13 @@
+package com.rebin.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rebin/booking/RebinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebin.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
