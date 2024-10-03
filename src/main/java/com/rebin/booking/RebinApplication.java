8a79512eda6d4046b0c38865c563b1d150562323
jueun oh <jueun1025@naver.com>
2024-10-03 10:17:13 +0900

    feat : jpa auditing 활성화

--- src/main/java/com/rebin/booking/RebinApplication.java
+++ src/main/java/com/rebin/booking/RebinApplication.java
@@ -2,8 +2,10 @@ package com.rebin.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RebinApplication {
 
     public static void main(String[] args) {
