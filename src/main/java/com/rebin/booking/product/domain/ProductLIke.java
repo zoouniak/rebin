7989b51ba2eace5e31141a747c1470646b020001
jueun oh <jueun1025@naver.com>
2024-10-14 19:33:39 +0900

    feat : 연관관계 제거

--- src/main/java/com/rebin/booking/product/domain/ProductLIke.java
+++ src/main/java/com/rebin/booking/product/domain/ProductLIke.java
@@ -1,23 +1,28 @@
 package com.rebin.booking.product.domain;
 
-import com.rebin.booking.member.domain.Member;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import static jakarta.persistence.CascadeType.REMOVE;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductLIke {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-    @ManyToOne(cascade = {REMOVE})
-    @JoinColumn(name = "member_id")
-    private Member member;
 
-    @ManyToOne(cascade = {REMOVE})
-    @JoinColumn(name = "product_id")
-    private Product product;
+    private Long member;
+
+    private Long product;
+
+    public ProductLIke(Long member, Long product) {
+        this.member = member;
+        this.product = product;
+    }
 }
