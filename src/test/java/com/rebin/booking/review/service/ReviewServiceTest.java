914a4855718f5d6d67e12b6d62f952eb1fe55eb2
jueun oh <jueun1025@naver.com>
2024-10-23 16:36:50 +0900

    test : 리뷰 조회, 수정, 삭제 단위 테스트

---
+++ src/test/java/com/rebin/booking/review/service/ReviewServiceTest.java
@@ -0,0 +1,87 @@
+package com.rebin.booking.review.service;
+
+import com.rebin.booking.common.excpetion.ReviewException;
+import com.rebin.booking.member.domain.Member;
+import com.rebin.booking.member.domain.repository.MemberRepository;
+import com.rebin.booking.member.type.ProviderType;
+import com.rebin.booking.product.domain.Product;
+import com.rebin.booking.reservation.domain.Reservation;
+import com.rebin.booking.reservation.domain.TimeSlot;
+import com.rebin.booking.reservation.domain.repository.ReservationRepository;
+import com.rebin.booking.reservation.domain.type.ReservationStatusType;
+import com.rebin.booking.reservation.dto.request.ReservationRequest;
+import com.rebin.booking.review.domain.Review;
+import com.rebin.booking.review.domain.repository.ReviewHelpRepository;
+import com.rebin.booking.review.domain.repository.ReviewRepository;
+import com.rebin.booking.review.dto.request.ReviewCreateRequest;
+import com.rebin.booking.review.dto.request.ReviewEditRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+
+import static com.rebin.booking.reservation.domain.type.ReservationStatusType.COMPLETED;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReviewServiceTest {
+    @InjectMocks
+    ReviewService reviewService;
+    @Mock
+    ReviewRepository reviewRepository;
+    @Mock
+    ReviewHelpRepository reviewHelpRepository;
+    @Mock
+    MemberRepository memberRepository;
+    @Mock
+    ReservationRepository reservationRepository;
+
+    @Test
+    void 이미_리뷰한_예약에_리뷰를_작성한다() {
+        ReviewCreateRequest request = new ReviewCreateRequest(1L, "리뷰 내용");
+        when(reviewRepository.existsByReservationId(any())).thenReturn(true);
+
+        ReviewException reviewException = assertThrows(ReviewException.class, () -> reviewService.createReview(1L, request));
+
+        Assertions.assertEquals("V001", reviewException.getCode());
+    }
+
+    @Test
+    void 존재하지않는_리뷰를_조회한다() {
+        when(reviewRepository.findById(1L)).thenReturn(Optional.empty());
+
+        ReviewException reviewException = assertThrows(ReviewException.class, () -> reviewService.getReview(1L, 1L));
+
+        Assertions.assertEquals("V002", reviewException.getCode());
+    }
+
+    @Test
+    void 자신이_작성하지않은_리뷰를_수정한다() {
+        when(reviewRepository.existsByIdAndMemberId(1L, 1L)).thenReturn(false);
+        ReviewEditRequest request = new ReviewEditRequest("내용입니다길게작성해야하지..");
+
+        ReviewException reviewException = assertThrows(ReviewException.class, () -> reviewService.editReview(1L, 1L, request.content()));
+
+        assertEquals("V002", reviewException.getCode());
+    }
+
+    @Test
+    void 자신이_작성하지않은_리뷰를_삭제한다(){
+        when(reviewRepository.existsByIdAndMemberId(1L, 1L)).thenReturn(false);
+
+        ReviewException reviewException = assertThrows(ReviewException.class, () -> reviewService.deleteReview(1L, 1L));
+
+        assertEquals("V002", reviewException.getCode());
+    }
+
+
+}
