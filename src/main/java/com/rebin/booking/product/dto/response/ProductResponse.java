a3ee7c319e0ecf04555d40089e424730c636bbdb
jueun oh <jueun1025@naver.com>
2024-11-01 17:04:21 +0900

    refactor : of 메서드 추가

--- src/main/java/com/rebin/booking/product/dto/response/ProductResponse.java
+++ src/main/java/com/rebin/booking/product/dto/response/ProductResponse.java
@@ -1,5 +1,7 @@
 package com.rebin.booking.product.dto.response;
 
+import com.rebin.booking.product.domain.Product;
+
 public record ProductResponse(
         Long id,
         String name,
@@ -7,4 +9,13 @@ public record ProductResponse(
         String summary,
         int price
 ) {
+    public static ProductResponse of(Product product){
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getThumbnail(),
+                product.getSummary(),
+                product.getPrice()
+        );
+    }
 }
