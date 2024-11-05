14620058b1a8504f34c33093e9c4a8367ec0ac89
jueun oh <jueun1025@naver.com>
2024-11-05 11:22:02 +0900

    fix : status 삭제

--- src/main/java/com/rebin/booking/common/excpetion/ErrorCode.java
+++ src/main/java/com/rebin/booking/common/excpetion/ErrorCode.java
@@ -1,41 +1,41 @@
 package com.rebin.booking.common.excpetion;
 
 public enum ErrorCode {
-    SERVER_ERROR(500, "S001", "err.server"),
-    INVALID_AUTHORITY(403, "A001", "err.invalid.authority"),
-    INVALID_REQUEST(400, "A002", "err.invalid.request"),
-    INVALID_ARGUMENT(400, "A003", "err.invalid.argument"),
+    SERVER_ERROR("S001", "err.server"),
+    INVALID_AUTHORITY("A001", "err.invalid.authority"),
+    INVALID_REQUEST("A002", "err.invalid.request"),
+    INVALID_ARGUMENT("A003", "err.invalid.argument"),
 
-    FAIL_GET_USERINFO(500, "L001", "err.fail.get-userinfo"),
-    INVALID_AUTHORIZE_CODE(500, "L002", "err.invalid.auth-code"),
-    NOT_SUPPORT_PROVIDER(400, "L003", "err.invalid.provider"),
-    EXPIRED_ACCESS_TOKEN(400, "L004", "err.expired.access-token"),
-    INVALID_TOKEN(400, "L005", "err.invalid.token"),
-    EXPIRED_REFRESH_TOKEN(400, "L006", "err.expired.refresh-token"),
-    INVALID_REFRESH_TOKEN(400, "L007", "err.invalid.refresh-token"),
-    FAIL_EXTEND(403, "L008", "err.fail.login-extend"),
-    INVALID_PRODUCT(400, "P001", "err.invalid.product"),
-    INVALID_TIMESLOT(400, "T001", "err.invalid.timeslot"),
-    MAX_ATTEMPTS_EXCEEDED(500, "R001", "err.fail.reservation-code"),
-    RESERVATION_FULL(400, "R002", "err.already-full.timeslot"),
-    NOT_SUPPORT_STATUS(400, "R003", "err.invalid.status"),
-    INVALID_RESERVATION(400, "R004", "err.invalid.reservation"),
-    CANT_CANCEL(400,"R005","err.fail-cancel.reservation"),
-    ALREADY_WRITE(400,"V001","err.already-write.review"),
-    INVALID_REVIEW(400,"V002","err.invalid.review");
-    private final int status;
+    FAIL_GET_USERINFO("L001", "err.fail.get-userinfo"),
+    INVALID_AUTHORIZE_CODE("L002", "err.invalid.auth-code"),
+    NOT_SUPPORT_PROVIDER("L003", "err.invalid.provider"),
+    EXPIRED_ACCESS_TOKEN("L004", "err.expired.access-token"),
+    INVALID_TOKEN("L005", "err.invalid.token"),
+    EXPIRED_REFRESH_TOKEN("L006", "err.expired.refresh-token"),
+    INVALID_REFRESH_TOKEN("L007", "err.invalid.refresh-token"),
+    FAIL_EXTEND("L008", "err.fail.login-extend"),
+    INVALID_PRODUCT("P001", "err.invalid.product"),
+    INVALID_TIMESLOT("T001", "err.invalid.timeslot"),
+    MAX_ATTEMPTS_EXCEEDED("R001", "err.fail.reservation-code"),
+    RESERVATION_FULL("R002", "err.already-full.timeslot"),
+    NOT_SUPPORT_STATUS("R003", "err.invalid.status"),
+    INVALID_RESERVATION("R004", "err.invalid.reservation"),
+    CANT_CANCEL("R005", "err.fail-cancel.reservation"),
+    ALREADY_WRITE("V001", "err.already-write.review"),
+    INVALID_REVIEW("V002", "err.invalid.review"),
+    INVALID_IMG_PATH("I001", "err.invalid.path"),
+    INVALID_IMAGE("I002","err.invalid.image"),
+    EXCEED_IMAGE_LIST_SIZE("I003","err.exceed.image-size"),
+    EMPTY_IMAGE_LIST("I004","err.empty.image-size");
+
     private final String code;
     private final String msg;
 
 
-    ErrorCode(int status, String code, String msg) {
-        this.status = status;
+    ErrorCode(String code, String msg) {
+
         this.code = code;
         this.msg = msg;
-    }
-
-    public int getStatus() {
-        return status;
     }
 
     public String getCode() {
