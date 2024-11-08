66b0bcbeb0d80ac3837b1066b803404b9ea02c27
jueun oh <jueun1025@naver.com>
2024-11-08 20:15:34 +0900

    refactor : notice 요청 필드 제한

--- src/main/java/com/rebin/booking/notice/dto/request/NoticeRequest.java
+++ src/main/java/com/rebin/booking/notice/dto/request/NoticeRequest.java
@@ -1,7 +1,15 @@
 package com.rebin.booking.notice.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record NoticeRequest(
+        @Size(max = 20, message = "제목의 길이는 20자 이하여야합니다")
+        @NotNull
         String title,
+
+        @Size(max = 5000, message = "제목의 길이는 5000자 이하여야합니다")
+        @NotNull
         String content
 ) {
 }
