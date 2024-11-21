ec77e143c22fc8438e36bdc895089cd040edcc3f
jueun oh <jueun1025@naver.com>
2024-11-21 15:07:07 +0900

    refactor : swagger 명세 및 접근 제어

--- src/main/java/com/rebin/booking/notice/presentation/AdminNoticeController.java
+++ src/main/java/com/rebin/booking/notice/presentation/AdminNoticeController.java
@@ -1,8 +1,12 @@
 package com.rebin.booking.notice.presentation;
 
+import com.rebin.booking.auth.domain.Accessor;
+import com.rebin.booking.auth.domain.AdminAuth;
+import com.rebin.booking.auth.domain.AdminOnly;
 import com.rebin.booking.notice.dto.request.NoticeRequest;
 import com.rebin.booking.notice.dto.response.NoticeResponse;
 import com.rebin.booking.notice.service.AdminNoticeService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,20 +18,32 @@ import org.springframework.web.bind.annotation.*;
 public class AdminNoticeController {
     private final AdminNoticeService adminNoticeService;
 
+    @Operation(summary = "공지사항 등록")
     @PostMapping
-    public ResponseEntity<NoticeResponse> saveNotice(@RequestBody @Valid NoticeRequest request) {
+    @AdminOnly
+    public ResponseEntity<NoticeResponse> saveNotice(
+            @AdminAuth Accessor accessor,
+            @RequestBody @Valid NoticeRequest request) {
         return ResponseEntity.ok(adminNoticeService.saveNotice(request));
     }
 
+    @Operation(summary = "공지사항 수정")
     @PutMapping("/{noticeId}")
-    public ResponseEntity<Void> updateNotice(@PathVariable Long noticeId,
-                                             @RequestBody @Valid NoticeRequest request){
+    @AdminOnly
+    public ResponseEntity<Void> updateNotice(
+            @AdminAuth Accessor accessor,
+            @PathVariable Long noticeId,
+            @RequestBody @Valid NoticeRequest request) {
         adminNoticeService.updateNotice(noticeId, request);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "공지사항 삭제")
     @DeleteMapping("/{noticeId}")
-    public ResponseEntity<Void> deleteNotice(@PathVariable Long noticeId){
+    @AdminOnly
+    public ResponseEntity<Void> deleteNotice(
+            @AdminAuth Accessor accessor,
+            @PathVariable Long noticeId) {
         adminNoticeService.deleteNotice(noticeId);
         return ResponseEntity.noContent().build();
     }
