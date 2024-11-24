d5c2e8b009d681b85298664f174e47dcaf0456c1
jueun oh <jueun1025@naver.com>
2024-11-24 19:04:06 +0900

    refactor : 생성자 추가

--- src/main/java/com/rebin/booking/admin/domain/Admin.java
+++ src/main/java/com/rebin/booking/admin/domain/Admin.java
@@ -4,12 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Admin {
     @Id
     @GeneratedValue(strategy = IDENTITY)
@@ -20,4 +23,10 @@ public class Admin {
 
     @Column
     private String password;
+
+    public Admin(Long id, String loginId, String password) {
+        this.id = id;
+        this.loginId = loginId;
+        this.password = password;
+    }
 }
