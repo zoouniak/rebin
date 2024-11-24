7d402fb40623941f06d2ae48f6df7d285a3dccd0
jueun oh <jueun1025@naver.com>
2024-11-24 19:30:32 +0900

    chore : 파일 위치 이동

--- src/main/java/com/rebin/booking/image/presentation/ImageController.java
+++ src/main/java/com/rebin/booking/image/presentation/ImageController.java
@@ -1,6 +1,6 @@
 package com.rebin.booking.image.presentation;
 
-import com.rebin.booking.image.ImageService;
+import com.rebin.booking.image.service.ImageService;
 import com.rebin.booking.image.dto.response.ImageResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
