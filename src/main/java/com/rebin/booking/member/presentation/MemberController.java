dcdefcd910b5690e7899e8aebc1ac951b7128afa
jueun oh <jueun1025@naver.com>
2024-11-24 11:42:57 +0900

    fix : ResponseEntity 반환

--- src/main/java/com/rebin/booking/member/presentation/MemberController.java
+++ src/main/java/com/rebin/booking/member/presentation/MemberController.java
@@ -10,6 +10,7 @@ import com.rebin.booking.member.dto.response.MemberResponse;
 import com.rebin.booking.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,24 +29,27 @@ public class MemberController {
     @Operation(summary = "이름 변경")
     @PatchMapping("/name")
     @MemberOnly
-    public void editName(@Auth Accessor accessor,
-                         @RequestBody NameRequest request) {
+    public ResponseEntity<Void> editName(@Auth Accessor accessor,
+                                         @RequestBody NameRequest request) {
         memberService.editName(accessor.getMemberId(), request.name());
+        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "전화번호 변경")
     @PatchMapping("/phone")
     @MemberOnly
-    public void editPhone(@Auth Accessor accessor,
+    public ResponseEntity<Void> editPhone(@Auth Accessor accessor,
                          @RequestBody PhoneRequest request) {
         memberService.editPhone(accessor.getMemberId(), request.phone());
+        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "닉네임 변경")
     @PatchMapping("/nickname")
     @MemberOnly
-    public void editNickname(@Auth Accessor accessor,
+    public ResponseEntity<Void> editNickname(@Auth Accessor accessor,
                          @RequestBody NicknameRequest request) {
         memberService.editNickname(accessor.getMemberId(), request.nickname());
+        return ResponseEntity.ok().build();
     }
 }
