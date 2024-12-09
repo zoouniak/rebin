473f2547b326e4e275f489a009f81d1887c89f61
jueun oh <jueun1025@naver.com>
2024-12-09 15:37:16 +0900

    fix : 예약 검증 메서드 구체화

--- src/main/java/com/rebin/booking/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/com/rebin/booking/reservation/domain/repository/ReservationRepository.java
@@ -1,6 +1,7 @@
 package com.rebin.booking.reservation.domain.repository;
 
 import com.rebin.booking.reservation.domain.Reservation;
+import com.rebin.booking.reservation.domain.type.ReservationStatusType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,6 +11,7 @@ import java.util.Optional;
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
     Optional<Reservation> findByCode(String code);
     boolean existsByMemberIdAndId(Long memberId, Long reservationId);
+    boolean existsByMemberIdAndIdAndStatusIn(Long memberId, Long reservationId, List<ReservationStatusType> status);
     @Query("""
             select r from Reservation r where r.member.id = :memberId 
             and (r.status = 'PENDING_PAYMENT' or r.status = 'CONFIRM_REQUESTED'or r.status = 'PAYMENT_CONFIRMED')
